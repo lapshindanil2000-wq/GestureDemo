c3e6598efd663a8969cbdc57f4deeb1c301fab51
ComposeDemo <danil.lapshin.2000@mail.ru>
2026-04-18 16:03:18 +0700

    gestures

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,10 +49,12 @@ fun MainScreen(modifier: Modifier = Modifier) {
 fun MultiTouchDemo(modifier: Modifier = Modifier) {
     var scale by remember { mutableStateOf(1f) }
     var angle by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
 
-    val state = rememberTransformableState { scaleChange, _, rotationChange ->
+    val state = rememberTransformableState { scaleChange, offsetChange, rotationChange ->
         scale *= scaleChange
         angle += rotationChange
+        offset += offsetChange
     }
 
     Box(contentAlignment = Alignment.Center, modifier = modifier.fillMaxSize()) {
@@ -60,7 +63,9 @@ fun MultiTouchDemo(modifier: Modifier = Modifier) {
                 .graphicsLayer(
                     scaleX = scale,
                     scaleY = scale,
-                    rotationZ = angle
+                    rotationZ = angle,
+                    translationX = offset.x,
+                    translationY = offset.y
                 )
                 .transformable(state = state)
                 .background(Color.Blue)
